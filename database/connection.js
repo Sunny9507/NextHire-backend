4ae040af270665c4a0b562e144230475bc4577fa
Sunny <sunny950775@gmail.com>
2025-11-08 15:16:05 +0530

    backend pushing

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connection = ()=>{
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "job_portal"
+    }).then(()=>{
+        console.log("Connected to database.")
+    }).catch(err=>{
+        console.log(`Some error occured while connecting to database: ${err}`)
+    })
+}
